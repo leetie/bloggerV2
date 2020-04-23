ae5b6d906d74e31381cb987cf61f174bc08b84e1
Jesse Parsons <savvyjesse@aol.com>
2020-04-23 18:32:20 -0400

    add tagging feature with edit/destroy functionality

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,38 @@
+class TagsController < ApplicationController
+  include TagsHelper
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+    @tag.destroy
+    redirect_to tags_path
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.create(tag_params)
+    flash.notice = "Tag '#{@tag.name}' Created!"
+    redirect_to tags_path
+  end
+  
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    @tag.update(tag_params)
+    flash.notice = "Tag '#{@tag.name}' Updated!"
+    redirect_to tag_path(@tag)
+  end
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect {|tag| tag.name}.join(", ")
+  end
+
+  def tag_list=(str)
+    tag_names = str.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_existing_tags = tag_names.collect {|name| Tag.find_or_create_by(name: name)}
+    self.tags = new_or_existing_tags
+  end
 end
